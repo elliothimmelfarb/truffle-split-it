47463dd2167527f7125f0ff0442c1e7f118b6223
Kevin Healy <kevinhealy@Kevins-MacBook-Pro-2.local>
2018-03-15 07:57:15 -0700

    added quick one line description

--- src/About/About.js
+++ src/About/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        About
+        <h1>Create your own smart contract that will split ether between up to 10 ethereum user accounts.</h1>
       </div>
     );
   }
