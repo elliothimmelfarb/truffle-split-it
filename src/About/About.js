2e553017d602045f43e821947d175444a56d375f
Kevin Healy <kevinhealy@Kevins-MacBook-Pro-2.local>
2018-03-15 08:27:36 -0700

    imported top level components and added some filler text

--- src/About/About.js
+++ src/About/About.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  PaddingContainer,
+  TopArea,
+  PageTitle,
+  BaseButtonBlue,
+  ContentArea,
+  NotConnectedPane,
+} from '../components/TopLevelComponents'
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>Create your own smart contract that will split ether between up to 10 ethereum user accounts.</h1>
-      </div>
+      <Container>
+        <PaddingContainer>
+          <TopArea>
+        <PageTitle>Create your own smart contract that will split ether between up to 10 ethereum user accounts.</PageTitle>
+          </TopArea>
+
+          <ContentArea>
+            <p>This is content</p>
+          </ContentArea>
+      </PaddingContainer>
+    </Container>
     );
   }
 }
