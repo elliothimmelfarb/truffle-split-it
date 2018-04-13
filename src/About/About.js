acbd16cf2471cb74f915556bd2706db1be8e6b23
Kevin Healy <kevinhealy@Kevins-MBP-2.attlocal.net>
2018-04-12 21:21:47 -0700

    Created basic structure

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import {
   Container,
   PaddingContainer,
@@ -7,22 +8,48 @@ import {
   BaseButtonBlue,
   ContentArea,
   NotConnectedPane,
-} from '../components/TopLevelComponents'
+} from '../components/TopLevelComponents';
+
+export const AboutContainer = styled.main`
+  display: block;
+  background-color: lightgray;
+  width: 100%;
+  height: 100%;
+`
+
+export const CoverContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: white;
+width: 100%;
+height: 40vh;
+`
+export const CoverAnimation = styled.img`
+flex-shrink: 0;
+max-height: 100%;
+max-width: 100%;
+`
+
+export const Headline = styled.h1`
+  color: black;
+  width: 80%;
+  padding-left: 10%;
+  padding-right: 10%;
+  text-align: center;
+`
 
 class About extends Component {
   render() {
     return (
       <Container>
-        <PaddingContainer>
-          <TopArea>
-        <PageTitle>Create your own smart contract that will split ether between up to 10 ethereum user accounts.</PageTitle>
-          </TopArea>
-
-          <ContentArea>
-            <p>This is content</p>
-          </ContentArea>
-      </PaddingContainer>
-    </Container>
+        <AboutContainer>
+          <CoverContainer>
+            <CoverAnimation src="https://media.giphy.com/media/2UJahanu04M5G/giphy.gif"/>
+          </CoverContainer>
+          <Headline>Create your own ethereum smart contract that splits ether between multiple accounts.</Headline>
+        </AboutContainer>
+      </Container>
     );
   }
 }
